8dfac77a349e05446bb516fd0e7bff4407a44f7e
Jipeng Li <54520548+HHyacinth@users.noreply.github.com>
2019-09-30 10:10:09 +0800

    Create 头文件.cpp

---
+++ 头文件.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INF 0x3f3f3f3f
+#define INFll 0x3f3f3f3f3f3f3f3f
+#define ll long long
+#define ul unsigned long long
+#define pii pair<int ,int >
+#define pll pair<ll , ll >
+#define pb(x) push_back(x)
+#define ls(x) (x<<1)
+#define rs(x) (x<<1|1)
+#define GCD(a,b) __gcd(a,b)
+#define PI acos(-1)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define endl '\n'
+#define isLeap(x) (x%4==0&&x%100!=0||x%400==0)
+#define Close() std::ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+const int day[2][13]={{0,31,28,31,30,31,30,31,31,30,31,30,31},
+                      {0,31,29,31,30,31,30,31,31,30,31,30,31}/**leap*/};
+const int dx[8]={1,-1,0,0,1,1,-1,-1};
+const int dy[8]={0,0,1,-1,1,-1,1,-1};
+const int MOD=1e9+7;
+const int maxn=1e5+50;
+
+int main()
+{
+
+}
